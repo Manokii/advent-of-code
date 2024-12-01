f99e241962d4c453466940f8bc235899df0d1727
Jasper Concepcion <jasper@manok.dev>
2024-12-01 18:26:04 +0800

    chore(ts): cleanup 2024 day1

--- ts/solutions/2024/1/index.ts
+++ ts/solutions/2024/1/index.ts
@@ -1,40 +1,31 @@
-import { AventOfCode, tyep Part } from "../../../lib/advent-of-code";
+import { AventOfCode, Part } from "../../../lib/advent-of-code";
 
-function getData(lines: string[]) {
-	const data = lines.reduce<{ x: number[]; y: number[] }>(
-		(acc, cur) => {
-			const regex = /(\d+)\s+(\d+)/;
-			const match = cur.match(regex);
+function parseInput(lines: string[]) {
+	const x: number[] = [];
+	const y: number[] = [];
 
-			if (!match) {
-				throw new Error(`Invalid input: ${cur}`);
-			}
+	for (const line of lines) {
+		const regex = /(\d+)\s+(\d+)/;
+		const match = line.match(regex) ?? [];
+		const [_, left, right] = match.map(Number);
+		if (left === undefined || right === undefined) {
+			throw new Error(`Invalid input: ${line}`);
+		}
+		x.push(left);
+		y.push(right);
+	}
 
-			const [_, x, y] = match.map(Number);
-
-			if (isNaN(x) || isNaN(y)) {
-				throw new Error(`Invalid input: ${cur}`);
-			}
-
-			acc.x.push(x);
-			acc.y.push(y);
-			return acc;
-		},
-		{ x: [], y: [] },
-	);
-
-	return data;
+	return { x, y };
 }
 
 export default async function main() {
 	const day1 = new AventOfCode(2024, 1);
 	await day1.init();
+	const parsedInput = parseInput(day1.input);
 
-	// Day 1 part 1 - get diff of sorted arrays
-	day1.addSolution(Part.Part1, (lines) => {
-		const data = getData(lines);
-		const xSorted = data.x.sort((a, b) => a - b);
-		const ySorted = data.y.sort((a, b) => a - b);
+	day1.addSolution(Part.Part1, () => {
+		const xSorted = parsedInput.x.sort((a, b) => a - b);
+		const ySorted = parsedInput.y.sort((a, b) => a - b);
 		let diff = 0;
 		for (let i = 0; i < xSorted.length; i++) {
 			diff += Math.abs(xSorted[i] - ySorted[i]);
@@ -42,13 +33,11 @@ export default async function main() {
 		return diff;
 	});
 
-	// Day 1 part 2 - get similarity score
-	day1.addSolution(Part.Part2, (lines) => {
-		const data = getData(lines);
+	day1.addSolution(Part.Part2, () => {
 		let similarityScore = 0;
-		for (let i = 0; i < data.x.length; i++) {
-			const x = data.x[i];
-			const xInY = data.y.filter((y) => y === x).length;
+		for (let i = 0; i < parsedInput.x.length; i++) {
+			const x = parsedInput.x[i];
+			const xInY = parsedInput.y.filter((y) => y === x).length;
 			similarityScore += x * xInY;
 		}
 		return similarityScore;
